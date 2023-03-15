4ed26640786e0ae489caea57bf9ad164a32b5e5e
Michael Helvey <michael.helvey1@gmail.com>
2023-03-15 18:01:08 -0500

    Created working remix app

---
+++ server/main.ts
@@ -0,0 +1,42 @@
+import Koa from "koa";
+import serve from "koa-static";
+import path from "path";
+import logger from "koa-logger";
+import { createRequestHandler } from "remix-koa-adapter";
+
+const app = new Koa();
+
+const BUILD_DIR = path.join(process.cwd(), "build");
+
+app.use(logger());
+app.use(serve("public"));
+
+const remixAppHandler = createRequestHandler({
+  build: require(BUILD_DIR),
+  mode: process.env.NODE_ENV,
+});
+
+if (process.env.NODE_ENV === "development") {
+  app.use(async (ctx) => {
+    purgeRequireCache();
+    return remixAppHandler(ctx);
+  });
+} else {
+  app.use(remixAppHandler);
+}
+
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.log(`✅ App listening on port ${port}`);
+});
+
+/**
+ * Purge require cache on request for "server side HMR"
+ */
+function purgeRequireCache() {
+  for (const key in require.cache) {
+    if (key.startsWith(BUILD_DIR)) {
+      delete require.cache[key];
+    }
+  }
+}
